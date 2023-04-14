9867ce753e171977c0803e83ccde6c8e7aab48f2
georgelacademico <george.lima@academico.ifpb.edu.br>
2023-04-14 20:25:22 -0300

    Versão inicial pendendo revisão e novos recursos.

---
+++ main.js
@@ -0,0 +1,14 @@
+let body = document.querySelector('body')
+let toggle = document.getElementById('toggle')
+toggle.addEventListener('click', changeTheme)
+let slider = document.getElementById('slider')
+
+function changeTheme () {
+    if (!toggle.checked) {
+        body.style.backgroundColor = '#ffffff' //Light
+        slider.style.backgroundColor = '#ccc'
+    } else {
+        body.style.backgroundColor = '#1f1f1f' //Dark
+        slider.style.backgroundColor = '#2196F3'
+    }
+}
